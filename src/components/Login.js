bb66961117ae4035548367ebb50119d2d398777b
siddharth <siddharthgusain26011999@gmail.com>
2021-06-09 22:18:07 +0530

    added logincard component

---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { GoogleOutlined ,  FacebookOutlined } from '@ant-design/icons';
+
+const Login = () => {
+    return (
+        <div id= "login-page">
+            <div id="login-card">
+                <h2>Welcome to Let'sChatApp !</h2>
+
+                <div
+                    className ="login-button google"
+                >
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Login;
